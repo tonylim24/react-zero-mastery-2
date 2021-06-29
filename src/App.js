5382b5809d6c6829fa3aca1d2ac5ff1cf6c286dc
Tony Lim <tonolim24@gmail.com>
2021-06-29 15:03:32 -0700

    Added header component.

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import "./App.css";
 import Homepage from "./pages/Homepage/Homepage";
 import Shop from "./pages/shop/shop";
 import { Route, Switch } from "react-router-dom";
+import Header from "./components/header/header";
 
 function App() {
   return (
     <div>
+      <Header />
       <Switch>
         <Route exact path="/" component={Homepage} />
         <Route exact path="/shop" component={Shop} />
